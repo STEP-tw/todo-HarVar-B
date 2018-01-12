6ecc830ed65696fa4060af9d87733fbfff05144a
HarVar-B <harvar009@gmail.com>
2018-01-12 13:39:04 +0530

    added Item class object, along a passing test.

---
+++ src/item.js
@@ -0,0 +1,26 @@
+class Item{
+  constructor(name,description){
+    this.name=name;
+    this.description=description;
+    this.done=false;
+  }
+  get getName(){
+    return this.name;
+  }
+  get getDescription(){
+    return this.description;
+  }
+  get isDone(){
+    return this.done;
+  }
+  setDescription(description){
+    this.description=description;
+  }
+  tick(){
+    this.done=true;
+  }
+  untick(){
+    this.done=false;
+  }
+}
+module.exports=Item;
---
+++ test/itemTest.js
@@ -0,0 +1,34 @@
+const Item = require('../src/item.js');
+const assert = require('chai').assert;
+
+
+describe("item class object should:",()=>{
+  beforeEach(()=>{
+    item = new Item("item_name");
+  });
+  
+  it("be able to tell whether it is done or not.",()=>{
+    assert.isNotOk(item.isDone);
+    item.tick();
+    assert.isOk(item.isDone);
+    item.untick();
+  });
+
+  it("give it's name when asked for",()=>{
+    let expected = "item_name"
+    assert.equal(item.getName,expected);
+  });
+
+  it("give it's description when asked for",()=>{
+    let item = new Item("item_name","using for a test");
+    let expected = "using for a test";
+    assert.deepEqual(item.getDescription,expected);
+  });
+
+  it("be able to set own description",()=>{
+    item.setDescription("descibing after creation of object");
+    let expected = "descibing after creation of object";
+    assert.deepEqual(item.getDescription,expected);
+
+  });
+});
